c48d43bc0722cf089ed9b6ca29901edb7c92ef55
Nemanja Mijailovic <metalnem@mijailovic.net>
2018-06-09 11:13:12 +0200

    Add BouncyCastle to the mix

--- Intrinsics/AesBenchmark.cs
+++ Intrinsics/AesBenchmark.cs
@@ -2,6 +2,10 @@ using System.Security.Cryptography;
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Attributes.Jobs;
 using BenchmarkDotNet.Running;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Engines;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Security;
 
 namespace Intrinsics
 {
@@ -16,11 +20,30 @@ namespace Intrinsics
 
 		private readonly ICryptoTransform openSsl = Aes.Create().CreateEncryptor();
 		private readonly AesArm64 intrinsics = new AesArm64();
+		private readonly IBufferedCipher bouncySlow;
+		private readonly IBufferedCipher bouncyFast;
+
+		public AesBenchmark()
+		{
+#pragma warning disable 0618
+			var engine = new AesFastEngine();
+#pragma warning restore 0618
+
+			bouncySlow = CipherUtilities.GetCipher("AES/ECB/NoPadding");
+			bouncySlow.Init(true, new KeyParameter(new byte[16]));
+
+			bouncyFast = new BufferedBlockCipher(engine);
+			bouncyFast.Init(true, new KeyParameter(new byte[16]));
+		}
 
 		[Benchmark] public void OpenSsl1K() => openSsl.TransformBlock(input1K, 0, input1K.Length, output1K, 0);
 		[Benchmark] public void Intrinsics1K() => intrinsics.Encrypt(input1K, output1K);
+		[Benchmark] public void BouncyCastleSlow1K() => bouncySlow.ProcessBytes(input1K, 0, input1K.Length, output1K, 0);
+		[Benchmark] public void BouncyCastleFast1K() => bouncyFast.ProcessBytes(input1K, 0, input1K.Length, output1K, 0);
 
 		[Benchmark] public void OpenSsl1M() => openSsl.TransformBlock(input1M, 0, input1M.Length, output1M, 0);
 		[Benchmark] public void Intrinsics1M() => intrinsics.Encrypt(input1M, output1M);
+		[Benchmark] public void BouncyCastleSlow1M() => bouncySlow.ProcessBytes(input1M, 0, input1M.Length, output1M, 0);
+		[Benchmark] public void BouncyCastleFast1M() => bouncyFast.ProcessBytes(input1M, 0, input1M.Length, output1M, 0);
 	}
 }
