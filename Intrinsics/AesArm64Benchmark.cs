dbe9fe33f6a2ee95f54a0b04685ca904e9e48c4f
Nemanja Mijailovic <metalnem@mijailovic.net>
2018-06-09 09:58:37 +0200

    Refactor and add benchmarks

---
+++ Intrinsics/AesArm64Benchmark.cs
@@ -0,0 +1,26 @@
+using System.Security.Cryptography;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Attributes.Jobs;
+using BenchmarkDotNet.Running;
+
+namespace Intrinsics
+{
+	[InProcess]
+	public class AesArm64Benchmark
+	{
+		private static readonly byte[] input1K = new byte[1024];
+		private static readonly byte[] output1K = new byte[input1K.Length];
+
+		private static readonly byte[] input1M = new byte[1024 * 1024];
+		private static readonly byte[] output1M = new byte[input1M.Length];
+
+		private readonly ICryptoTransform openSsl = Aes.Create().CreateEncryptor();
+		private readonly AesArm64 intrinsics = new AesArm64();
+
+		[Benchmark] public void OpenSsl1K() => openSsl.TransformBlock(input1K, 0, input1K.Length, output1K, 0);
+		[Benchmark] public void Intrinsics1K() => intrinsics.Encrypt(input1K, output1K);
+
+		[Benchmark] public void OpenSsl1M() => openSsl.TransformBlock(input1M, 0, input1M.Length, output1M, 0);
+		[Benchmark] public void Intrinsics1M() => intrinsics.Encrypt(input1M, output1M);
+	}
+}
